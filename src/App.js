01cf3a3fe856e375b93a5d4eed0b776123b860bd
CodinMaster <latheyapoorv@gmail.com>
2021-02-28 20:48:41 +0530

    stylize header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 // Material UI
-import { Grid, TextField, Button } from "@material-ui/core";
+import { Grid, Box, TextField, Button } from "@material-ui/core";
 // Components
 import ConnectWallet from "./components/ConnectWallet";
 // ABIs
@@ -16,37 +16,42 @@ function App() {
   const [lpAmount, setLpAmount] = useState("");
   const [token0Share, setToken0Share] = useState("");
   const [token1Share, setToken1Share] = useState("");
-  const [token0Name, setToken0Name] = useState("")
-  const [token1Name, setToken1Name] = useState("")
+  const [token0Name, setToken0Name] = useState("");
+  const [token1Name, setToken1Name] = useState("");
 
   const calculate = async () => {
-    const uniPair = new web3.eth.Contract(uniPairABI, pairAddress);
-    const token0Address = await uniPair.methods.token0().call();
-    const token1Address = await uniPair.methods.token1().call();
+    try {
+      const uniPair = new web3.eth.Contract(uniPairABI, pairAddress);
+      const token0Address = await uniPair.methods.token0().call();
+      const token1Address = await uniPair.methods.token1().call();
 
-    const token0Instance = new web3.eth.Contract(tokenABI, token0Address);
-    const token1Instance = new web3.eth.Contract(tokenABI, token1Address);
+      const token0Instance = new web3.eth.Contract(tokenABI, token0Address);
+      const token1Instance = new web3.eth.Contract(tokenABI, token1Address);
 
-    const reserve0 = await token0Instance.methods.balanceOf(pairAddress).call();
-    const reserve1 = await token1Instance.methods.balanceOf(pairAddress).call();
+      const reserve0 = await token0Instance.methods
+        .balanceOf(pairAddress)
+        .call();
+      const reserve1 = await token1Instance.methods
+        .balanceOf(pairAddress)
+        .call();
 
-    const lpTotalSupply = await uniPair.methods.totalSupply().call();
+      const lpTotalSupply = await uniPair.methods.totalSupply().call();
 
-    var userToken0Share = new BN(reserve0)
-      .mul(new BN(lpAmount).mul(new BN("10").pow(new BN("18"))))
-      .div(new BN(lpTotalSupply));
-    var userToken1Share = new BN(reserve1)
-      .mul(new BN(lpAmount).mul(new BN("10").pow(new BN("18"))))
-      .div(new BN(lpTotalSupply));
+      const userToken0Share = new BN(reserve0)
+        .mul(new BN(lpAmount).mul(new BN("10").pow(new BN("18"))))
+        .div(new BN(lpTotalSupply));
+      const userToken1Share = new BN(reserve1)
+        .mul(new BN(lpAmount).mul(new BN("10").pow(new BN("18"))))
+        .div(new BN(lpTotalSupply));
 
-    userToken0Share = await toDecimal(token0Instance, userToken0Share);
-    userToken1Share = await toDecimal(token1Instance, userToken1Share);
+      setToken0Share(await toDecimal(token0Instance, userToken0Share));
+      setToken1Share(await toDecimal(token1Instance, userToken1Share));
 
-    setToken0Share(userToken0Share);
-    setToken1Share(userToken1Share);
-
-    setToken0Name(await token0Instance.methods.symbol().call())
-    setToken1Name(await token1Instance.methods.symbol().call())
+      setToken0Name(await token0Instance.methods.symbol().call());
+      setToken1Name(await token1Instance.methods.symbol().call());
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   const toDecimal = async (tokenInstance, amount) => {
@@ -67,9 +72,71 @@ function App() {
 
   return (
     <Grid container direction="column">
-      <Grid item>
-        <Grid container justify="center">
-          <ConnectWallet setWeb3={setWeb3} setAccount={setAccount} />
+      <Grid
+        container
+        style={{
+          marginBottom: "40px",
+          marginTop: "40px",
+        }}
+      >
+        <Grid item xs={4} />
+        <Grid item xs={4}>
+          <Box
+            fontWeight="fontWeightBold"
+            fontSize="h4.fontSize"
+            fontFamily="fontFamily"
+            fontStyle=""
+            style={{
+              margin: "auto",
+              color: "#673ab7",
+              borderBottom: "4px solid #01579b"
+            }}
+          >
+            LP Underlying Calculator
+          </Box>
+        </Grid>
+        <Grid item xs={4}>
+          {!web3 ? (
+            <Grid
+              container
+              justify="flex-end"
+              style={{
+                paddingRight: "2rem",
+              }}
+            >
+              <ConnectWallet setWeb3={setWeb3} setAccount={setAccount} />
+            </Grid>
+          ) : (
+            <>
+              <Grid container justify="center">
+                <Grid item>
+                  <Box
+                    fontWeight="fontWeightBold"
+                    fontSize="1.2rem"
+                    fontFamily="fontFamily"
+                    fontStyle=""
+                    style={{
+                      color: "green",
+                      marginRight: "1rem",
+                    }}
+                  >
+                    • Connected
+                  </Box>
+                </Grid>
+                <Grid item>
+                  <Box
+                    fontWeight="fontWeightBold"
+                    fontFamily="fontFamily"
+                    style={{
+                      color: "green",
+                    }}
+                  >
+                    {account}
+                  </Box>
+                </Grid>
+              </Grid>
+            </>
+          )}
         </Grid>
       </Grid>
       <Grid item>
@@ -86,6 +153,7 @@ function App() {
           style={{
             minWidth: "450px",
           }}
+          autoComplete="off"
           onChange={(e) => setPairAddress(e.target.value)}
         />
       </Grid>
@@ -97,6 +165,7 @@ function App() {
           style={{
             minWidth: "450px",
           }}
+          autoComplete="off"
           onChange={(e) => setLpAmount(e.target.value)}
         />
       </Grid>
@@ -113,8 +182,12 @@ function App() {
           Calculate
         </Button>
       </Grid>
-      <Grid item>Token A Val: {token0Share} {token0Name}</Grid>
-      <Grid item>Token B Val: {token1Share} {token1Name}</Grid>
+      <Grid item>
+        Token A Val: {token0Share} {token0Name}
+      </Grid>
+      <Grid item>
+        Token B Val: {token1Share} {token1Name}
+      </Grid>
     </Grid>
   );
 }
