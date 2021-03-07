4ac511281aea5c0d4e6b2b2b278718bff916639c
CodinMaster <latheyapoorv@gmail.com>
2021-03-08 04:58:28 +0530

    Add footer

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import {
   Link,
   Typography,
 } from "@material-ui/core";
+import { Twitter, GitHub, LinkedIn, Language } from "@material-ui/icons";
 import axios from "axios";
 // Components
 import ConnectWallet from "./components/ConnectWallet";
@@ -738,6 +739,99 @@ function App() {
           </Grid>
         )}
       </Paper>
+      <Grid
+        container
+        style={{
+          marginTop: "30px",
+          paddingBottom: "40px",
+          paddingTop: "30px",
+          borderTop: "2px solid black",
+          backgroundColor: "black",
+        }}
+      >
+        <Grid item xs={3} />
+        <Grid
+          item
+          xs={6}
+          container
+          justify="center"
+          style={{
+            paddingRight: "2rem",
+          }}
+        >
+          <Grid container direction="column">
+            <Box
+              fontWeight="fontWeightBold"
+              fontSize="1.75rem"
+              fontFamily="fontFamily"
+              fontStyle=""
+              color="white"
+            >
+              Developed by: <Link
+                href="https://apoorvlathey.com/"
+                target="_blank"
+                rel="noopener"
+                style={{
+                  color: "white",
+                  textDecoration: "underline"
+                }}
+              >Apoorv Lathey</Link>
+            </Box>
+            <Grid
+              item
+              style={{
+                marginTop: "1rem",
+              }}
+            >
+              <Link
+                href="https://apoorvlathey.com/"
+                target="_blank"
+                rel="noopener"
+                style={{
+                  color: "white",
+                  padding: "1rem",
+                  paddingLeft: "0",
+                }}
+              >
+                <Language />
+              </Link>
+              <Link
+                href="https://twitter.com/apoorvlathey"
+                target="_blank"
+                rel="noopener"
+                style={{
+                  color: "white",
+                  padding: "1rem",
+                }}
+              >
+                <Twitter />
+              </Link>
+              <Link
+                href="https://github.com/CodinMaster"
+                target="_blank"
+                rel="noopener"
+                style={{
+                  color: "white",
+                  padding: "1rem",
+                }}
+              >
+                <GitHub />
+              </Link>
+              <Link
+                href="https://www.linkedin.com/in/apoorvlathey/"
+                target="_blank"
+                rel="noopener"
+                style={{
+                  color: "white",
+                  padding: "1rem",
+                }}
+              >
+                <LinkedIn />
+              </Link>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
     </Grid>
   );
 }
